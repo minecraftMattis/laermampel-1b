3e7b6c0900b976dbaefa35c48d3326dc5df0688d
minecraftMattis <mattis.lueger@icloud.com>
2024-06-24 14:46:51 +0200

    senden von lautstaerke

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 let lautstaerke = 0
+radio.setGroup(1)
 basic.forever(function () {
     lautstaerke = input.soundLevel()
-    if (lautstaerke > 160) {
+    if (lautstaerke > 134) {
+        radio.sendNumber(lautstaerke)
+        music.play(music.tonePlayable(988, music.beat(BeatFraction.Double)), music.PlaybackMode.InBackground)
         basic.showIcon(IconNames.Sad)
     } else if (lautstaerke > 60) {
         basic.showLeds(`
